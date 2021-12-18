4492c8899b54a63bb8d60c8136206fe8be62a6e2
ZiweiLiu <iriscloudlau@gmail.com>
2021-12-18 22:51:26 +0100

    new_jacc_loss

--- loss.py
+++ loss.py
@@ -78,7 +78,7 @@ class JaccLoss(nn.Module):
     def forward(self, inputs, targets, smooth=1):
         
         #comment out if your model contains a sigmoid or equivalent activation layer
-        inputs = F.sigmoid(inputs)       
+        # inputs = F.sigmoid(inputs)
         
         #flatten label and prediction tensors
         inputs = inputs.view(-1)
